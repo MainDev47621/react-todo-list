474466a1b4c23c6b2de3192c40a2af67fccd6879
WebDev47621 <webdev47621@gmail.com>
2021-08-16 21:34:32 -0700

    Implement Toggle Todo functionality

--- src/App.js
+++ src/App.js
@@ -13,6 +13,13 @@ function reducer(todos, action) {
   switch(action.type) {
     case ACTIONS.ADD_TODO:
       return [...todos, newTodo(action.payload.name)];
+    case ACTIONS.TOGGLE_TODO:
+      return todos.map(todo => {
+        if (todo.id === action.payload.id) {
+          return { ...todo, completed: !todo.completed }
+        }
+        return todo;
+      });
     case ACTIONS.DELETE_TODO:
       return todos.filter(todo => todo.id !== action.payload.id);
     default:
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -4,7 +4,8 @@ import { ACTIONS } from '../App';
 export function Todo({ todo, dispatch }) {
     return (
         <div>
-            <span>{todo.name}</span>
+            <span style={{color: todo.completed ? '#999' : '#000'}}>{todo.name}</span>
+            <button onClick={() => dispatch({ type: ACTIONS.TOGGLE_TODO, payload: { id: todo.id } })}>Complete Todo</button>
             <button onClick={() => dispatch({ type: ACTIONS.DELETE_TODO, payload: { id: todo.id } })}>Delete Todo</button>
         </div>
     );
